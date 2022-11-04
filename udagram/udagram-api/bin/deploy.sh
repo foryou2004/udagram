cb280d0aad315fad830667cece9d1bda0a696eb6
Ahmed Mohamed <forex.terad@gmail.com>
2022-11-04 20:44:23 +0200

    test 5

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
 
-eb init udagram-api-env --platform node.js --region us-east-1
-eb use udagram-api-env
+eb init udagram-env --platform node.js --region us-east-1
+eb use udagram-env
 
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_BUCKET=$AWS_BUCKET AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION AWS_PROFILE=$AWS_PROFILE AWS_REGION=$AWS_REGION AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL 
 
 
-eb deploy udagram-api-env
+eb deploy udagram-env
